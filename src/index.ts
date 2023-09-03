ba044226a38cdaf32ab06ab343cbbddde304069b
TheOreoTM <qwertzybad@gmail.com>
2023-09-03 23:21:50 +0500

    im gonna go dicko mode

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import { DugClient } from '#lib/DugClient';
 import '#lib/setup';
 
 const client = new DugClient();
+console.log(process.env.DISCORD_TOKEN);
 
 const main = async () => {
 	try {
 		client.logger.info('Logging in');
-		await client.login();
+		await client.login(process.env.DISCORD_TOKEN);
 		client.logger.info('logged in');
 	} catch (error) {
 		client.logger.fatal(error);
