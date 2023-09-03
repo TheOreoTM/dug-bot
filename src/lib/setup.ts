3a2e4dddbff52830de69d80e487b657371fc6a1a
TheOreoTM <qwertzybad@gmail.com>
2023-09-03 23:18:46 +0500

    env

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -5,14 +5,12 @@ import { ApplicationCommandRegistries, RegisterBehavior } from '@sapphire/framew
 import '@sapphire/plugin-logger/register';
 import { setup } from '@skyra/env-utilities';
 import * as colorette from 'colorette';
-import { join } from 'node:path';
-import { rootDir } from '#constants';
 
 // Set default behavior to bulk overwrite
 ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.BulkOverwrite);
 
 // Read env var
-setup({ path: join(rootDir, '.env') });
+setup();
 
 // Enable colorette
 colorette.createColors({ useColor: true });
