cf1789001ca301863283da371284b12d7d165720
TheOreoTM <qwertzybad@gmail.com>
2023-09-03 23:41:06 +0500

    yeahhhh

--- src/preconditions/Registered.ts
+++ src/preconditions/Registered.ts
@@ -1,21 +1,24 @@
 import { GuildMessage } from '#lib/types/Discord';
 import { ApplyOptions } from '@sapphire/decorators';
-import { Precondition } from '@sapphire/framework';
+import { Command, Precondition } from '@sapphire/framework';
 import type { ChatInputCommandInteraction, ContextMenuCommandInteraction, GuildMember } from 'discord.js';
 
 @ApplyOptions<Precondition.Options>({
 	position: 1
 })
 export class UserPrecondition extends Precondition {
-	public override messageRun(message: GuildMessage) {
+	public override messageRun(message: GuildMessage, command: Command) {
+		if (command.name === 'register') return this.ok();
 		return this.check(message.member);
 	}
 
 	public override chatInputRun(interaction: ChatInputCommandInteraction<'cached'>) {
+		if (interaction.commandName === 'register') return this.ok();
 		return this.check(interaction.member);
 	}
 
 	public override contextMenuRun(interaction: ContextMenuCommandInteraction<'cached'>) {
+		if (interaction.commandName === 'register') return this.ok();
 		return this.check(interaction.member);
 	}
 
