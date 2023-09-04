9bd34fba0b7fbc0dbb1812637cc12020492cd596
TheOreoTM <qwertzybad@gmail.com>
2023-09-04 11:41:56 +0500

    syer

--- src/lib/util/prisma.ts
+++ src/lib/util/prisma.ts
@@ -41,7 +41,7 @@ export const xprisma = new PrismaClient().$extends({
 export async function resetAutoIncrement() {
 	try {
 		await prisma.$queryRaw`
-     SELECT setval('"YourTableName_id_seq"', 1, false);
+     SELECT setval('"User_id_seq"', 1, false);
     `;
 
 		console.log('Auto-increment sequence reset successfully.');
