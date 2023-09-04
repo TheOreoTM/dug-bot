b9c115ed725775c20307c2522e228a5efb7c70bf
TheOreoTM <qwertzybad@gmail.com>
2023-09-04 14:09:50 +0500

    dump and dumper

---
+++ src/commands/faction-join.ts
@@ -0,0 +1,39 @@
+import { formatFailMessage } from '#lib/util/formatter';
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+
+@ApplyOptions<Command.Options>({
+	description: 'A basic slash command'
+})
+export class UserCommand extends Command {
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder //
+				.setName(this.name)
+				.setDescription(this.description)
+				.addStringOption((option) =>
+					option //
+						.setAutocomplete(true)
+						.setRequired(true)
+						.setName('faction')
+						.setDescription('The faction you want to join')
+				)
+		);
+	}
+
+	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		const factionId = interaction.options.getString('faction', true);
+		const faction = this.container.db.faction.findUnique({
+			where: {
+				id: Number(factionId)
+			}
+		});
+
+		if (!faction) {
+			interaction.reply({ content: formatFailMessage('That faction doesnt exist'), ephemeral: true });
+			return;
+		}
+
+		interaction.reply({ content: `\`\`\`json\n${JSON.stringify(faction, null, 2)}\`\`\`` });
+	}
+}
---
+++ src/interaction-handlers/factionSearch.ts
@@ -0,0 +1,31 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { InteractionHandler, InteractionHandlerTypes } from '@sapphire/framework';
+import { AutocompleteInteraction, type ApplicationCommandOptionChoiceData } from 'discord.js';
+
+@ApplyOptions<InteractionHandler.Options>({
+	interactionHandlerType: InteractionHandlerTypes.Autocomplete
+})
+export class AutocompleteHandler extends InteractionHandler {
+	public override async run(interaction: AutocompleteInteraction, result: ApplicationCommandOptionChoiceData[]) {
+		return interaction.respond(result);
+	}
+
+	public override async parse(interaction: AutocompleteInteraction) {
+		if (interaction.commandName !== 'faction-join') return this.none();
+		// Get the focussed (current) option
+		const focusedOption = interaction.options.getFocused(true);
+		// Ensure that the option name is one that can be autocompleted, or return none if not.
+		switch (focusedOption.name) {
+			case 'faction': {
+				const allFactions = await this.container.db.faction.findMany();
+				const factionNames = allFactions.map((faction) => {
+					return { name: faction.name, value: faction.id };
+				});
+
+				return this.some(factionNames);
+			}
+			default:
+				return this.none();
+		}
+	}
+}
