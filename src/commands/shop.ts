9003e47ed7ed49f85e917d798de70f4e01f1c251
TheOreoTM <qwertzybad@gmail.com>
2023-09-11 00:51:28 +0500

    test

--- src/commands/shop.ts
+++ src/commands/shop.ts
@@ -2,7 +2,7 @@ import { DugColors } from '#constants';
 import { ApplyOptions } from '@sapphire/decorators';
 import { PaginatedMessage } from '@sapphire/discord.js-utilities';
 import { Command } from '@sapphire/framework';
-import { EmbedBuilder } from 'discord.js';
+import { ButtonStyle, ComponentType, EmbedBuilder } from 'discord.js';
 
 @ApplyOptions<Command.Options>({
 	description: 'The Shop'
@@ -30,7 +30,28 @@ export class UserCommand extends Command {
 				template: new EmbedBuilder()
 					.setColor(DugColors.Default)
 					.setTitle('The Shop')
-					.setDescription(`Your balance: ${user.cash.toLocaleString()}`)
+					.setDescription(`Your balance: ${user.cash.toLocaleString()}`),
+				actions: [
+					{
+						customId: '@sapphire/paginated-messages.firstPage',
+						style: ButtonStyle.Secondary,
+						emoji: '🔙',
+						type: ComponentType.Button,
+						run: ({ handler }) => {
+							handler.index = 0;
+						}
+					},
+					{
+						customId: '@sapphire/paginated-messages.firstPage',
+						style: ButtonStyle.Secondary,
+						emoji: '🔛',
+						type: ComponentType.Button,
+						run: ({ handler, interaction }) => {
+							console.log(handler, interaction);
+							handler.index = 1;
+						}
+					}
+				]
 			});
 			paginatedShop
 				.addPageEmbed((embed) =>
@@ -44,7 +65,64 @@ export class UserCommand extends Command {
 				.addPageEmbed((embed) =>
 					embed //
 						.addFields({ name: 'Item3', value: 'kkk' })
-				);
+				)
+				.setActions([
+					{
+						customId: '@sapphire/paginated-messages.firstPage',
+						style: ButtonStyle.Secondary,
+						emoji: Emojis.Backward,
+						type: ComponentType.Button,
+						run: ({ handler, interaction }) => {
+							handler.index = 0;
+							this.updateComponents(handler, interaction);
+						}
+					},
+					{
+						customId: '@sapphire/paginated-messages.previousPage',
+						style: ButtonStyle.Secondary,
+						emoji: Emojis.Left,
+						type: ComponentType.Button,
+						run: ({ handler, interaction }) => {
+							if (handler.index === 0) {
+								handler.index = handler.pages.length - 1;
+							} else {
+								--handler.index;
+							}
+							paginatedShop.updateComponents(handler, interaction);
+						}
+					},
+					{
+						customId: '@sapphire/paginated-messages.stop',
+						style: ButtonStyle.Secondary,
+						emoji: '⏹️',
+						type: ComponentType.Button,
+						run: ({ collector }) => collector.stop()
+					},
+					{
+						customId: '@sapphire/paginated-messages.nextPage',
+						style: ButtonStyle.Secondary,
+						emoji: Emojis.Right,
+						type: ComponentType.Button,
+						run: ({ handler, interaction }) => {
+							if (handler.index === handler.pages.length - 1) {
+								handler.index = 0;
+							} else {
+								++handler.index;
+							}
+							this.updateComponents(handler, interaction);
+						}
+					},
+					{
+						customId: '@sapphire/paginated-messages.goToLastPage',
+						style: ButtonStyle.Secondary,
+						emoji: Emojis.Forward,
+						type: ComponentType.Button,
+						run: ({ handler, interaction }) => {
+							handler.index = handler.pages.length - 1;
+							this.updateComponents(handler, interaction);
+						}
+					}
+				]);
 
 			await paginatedShop.run(interaction, interaction.user);
 			return;
