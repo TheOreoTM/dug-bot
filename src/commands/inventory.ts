8ec0486c533cbcc8e672a4822bb5b6c44167fd7d
TheOreoTM <qwertzybad@gmail.com>
2023-10-13 20:59:18 +0500

    to a woman so heartless

--- src/commands/inventory.ts
+++ src/commands/inventory.ts
@@ -4,6 +4,7 @@ import { InventoryItemType } from '#lib/types/Data';
 import { groupItems } from '#lib/util/utils';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
+import { send } from '@sapphire/plugin-editable-commands';
 import { EmbedBuilder, type Message } from 'discord.js';
 
 @ApplyOptions<Command.Options>({
@@ -13,9 +14,16 @@ import { EmbedBuilder, type Message } from 'discord.js';
 export class UserCommand extends Command {
 	public override async messageRun(message: Message) {
 		const invItems: InventoryItemType[] = await this.container.db.user.getInventory(message.author.id);
+		const template = new EmbedBuilder().setTitle(`${message.author.username}'s Inventory`).setColor(DugColors.Default);
+
+		if (invItems.length === 0) {
+			send(message, {
+				embeds: [template.setDescription('Absolutely nothing...')]
+			});
+			return;
+		}
 		const groupedItems = groupItems(invItems);
 
-		const template = new EmbedBuilder().setTitle(`${message.author.username}'s Inventory`).setColor(DugColors.Default);
 		new PaginatedInventory(groupedItems).setTemplate(template).make().run(message, message.author);
 	}
 }
