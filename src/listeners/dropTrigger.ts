2c1a9b6c5345a3ae7394ed6df859e033a94bff50
TheOreoTM <qwertzybad@gmail.com>
2023-10-13 15:50:44 +0500

    fix: defer

--- src/listeners/dropTrigger.ts
+++ src/listeners/dropTrigger.ts
@@ -29,6 +29,7 @@ export class UserEvent extends Listener {
 		const collector = response.createMessageComponentCollector({ componentType: ComponentType.Button, max: 1, maxUsers: 1, idle: 60_000 });
 
 		collector.on('collect', async (i: ButtonInteraction) => {
+			i.deferReply();
 			const userId = i.user.id;
 			const itemValue = pickRandom(drop.items);
 			const itemData = AllItems.get(itemValue);
