3b7feed00ae0cacef6556b4c01241c55e4e31849
TheOreoTM <qwertzybad@gmail.com>
2023-10-13 22:15:45 +0500

    uzi

--- src/commands/trigger.ts
+++ src/commands/trigger.ts
@@ -1,14 +1,13 @@
 import { DropManager } from '#lib/classes/DropManager';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
-const manager = DropManager.getInstance();
 
 @ApplyOptions<Command.Options>({
 	description: 'A basic command'
 })
 export class UserCommand extends Command {
 	public override async messageRun() {
-		console.log('trigger');
+		const manager = DropManager.getInstance();
 		manager.allowDrop = true;
 		console.log(manager);
 		manager.performDropLogic();
--- src/listeners/triggerDrop.ts
+++ src/listeners/triggerDrop.ts
@@ -6,7 +6,7 @@ import { ApplyOptions } from '@sapphire/decorators';
 import { Listener } from '@sapphire/framework';
 import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, ComponentType, EmbedBuilder, userMention } from 'discord.js';
 
-@ApplyOptions<Listener.Options>({ event: DugEvents.TriggerDrop, name: DugEvents.TriggerDrop })
+@ApplyOptions<Listener.Options>({ event: DugEvents.TriggerDrop, name: 'Trigger Drop' })
 export class UserEvent extends Listener {
 	public override async run(drop: DropType) {
 		const channel = this.container.client.channels.cache.get('1138806085998874746');
