538224b4b068aad667381a0c0d0e1afd6209754a
TheOreoTM <qwertzybad@gmail.com>
2023-10-13 22:30:19 +0500

    member inv

--- src/commands/inventory.ts
+++ src/commands/inventory.ts
@@ -1,19 +1,21 @@
 import { DugColors } from '#constants';
 import { PaginatedInventory } from '#lib/classes/PaginatedInventory';
 import { InventoryItemType } from '#lib/types/Data';
+import { GuildMessage } from '#lib/types/Discord';
 import { groupItems } from '#lib/util/utils';
 import { ApplyOptions } from '@sapphire/decorators';
-import { Command } from '@sapphire/framework';
+import { Args, Command } from '@sapphire/framework';
 import { send } from '@sapphire/plugin-editable-commands';
-import { EmbedBuilder, type Message } from 'discord.js';
+import { EmbedBuilder } from 'discord.js';
 
 @ApplyOptions<Command.Options>({
 	description: 'Check your inventory',
 	aliases: ['inv']
 })
 export class UserCommand extends Command {
-	public override async messageRun(message: Message) {
-		const invItems: InventoryItemType[] = await this.container.db.user.getInventory(message.author.id);
+	public override async messageRun(message: GuildMessage, args: Args) {
+		const target = await args.pick('member').catch(() => message.member);
+		const invItems: InventoryItemType[] = await this.container.db.user.getInventory(target.id);
 		const template = new EmbedBuilder().setTitle(`${message.author.username}'s Inventory`).setColor(DugColors.Default);
 
 		if (invItems.length === 0) {
