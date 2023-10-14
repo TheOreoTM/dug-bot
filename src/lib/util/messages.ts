c1ed3153ad869110d15349b8229dea0ab115935a
TheOreoTM <qwertzybad@gmail.com>
2023-10-14 20:08:29 +0500

    format cooldown message

---
+++ src/lib/util/messages.ts
@@ -0,0 +1,48 @@
+import { MessageCreateOptions } from 'discord.js';
+import { Message } from 'discord.js';
+import { seconds } from './common';
+import { send } from '@sapphire/plugin-editable-commands';
+import { sleep } from '@sapphire/utilities';
+
+async function deleteMessageImmediately(message: Message): Promise<Message> {
+	return (message.deletable ? await message.delete().catch(() => null) : message) ?? message;
+}
+
+/**
+ * Deletes a message, skipping if it was already deleted, and aborting if a non-zero timer was set and the message was
+ * either deleted or edited.
+ *
+ * This also ignores the `UnknownMessage` error code.
+ * @param message The message to delete.
+ * @param time The amount of time, defaults to 0.
+ * @returns The deleted message.
+ */
+export async function deleteMessage(message: Message, time = 0): Promise<Message> {
+	if (!message.deletable) return message;
+	if (time === 0) return deleteMessageImmediately(message);
+
+	const lastEditedTimestamp = message.editedTimestamp;
+	await sleep(time);
+
+	// If it was deleted or edited, cancel:
+	if (!message.deletable || message.editedTimestamp !== lastEditedTimestamp) {
+		return message;
+	}
+
+	return deleteMessageImmediately(message);
+}
+
+/**
+ * Sends a temporary editable message and then floats a {@link deleteMessage} with the given `timer`.
+ * @param message The message to reply to.
+ * @param options The options to be sent to the channel.
+ * @param timer The timer in which the message should be deleted, using {@link deleteMessage}.
+ * @returns The response message.
+ */
+export async function sendTemporaryMessage(message: Message, options: string | MessageCreateOptions, timer = seconds(7)): Promise<Message> {
+	if (typeof options === 'string') options = { content: options };
+
+	const response = (await send(message, options)) as Message;
+	await deleteMessage(response, timer);
+	return response;
+}
--- src/listeners/commands/messageCommands/messageCommandDenied.ts
+++ src/listeners/commands/messageCommands/messageCommandDenied.ts
@@ -1,12 +1,34 @@
+import { seconds } from '#lib/util/common';
 import { formatFailMessage } from '#lib/util/formatter';
+import { sendTemporaryMessage } from '#lib/util/messages';
+import { DurationFormatter } from '@sapphire/duration';
 import type { Events, MessageCommandDeniedPayload } from '@sapphire/framework';
-import { Listener, type UserError } from '@sapphire/framework';
+import { Identifiers, Listener, type UserError } from '@sapphire/framework';
 
+const cooldownMessageCooldown = new Set();
 export class UserEvent extends Listener<typeof Events.MessageCommandDenied> {
-	public override async run({ context, message: content }: UserError, { message }: MessageCommandDeniedPayload) {
+	public override async run({ context, message: content, identifier }: UserError, { message }: MessageCommandDeniedPayload) {
 		// `context: { silent: true }` should make UserError silent:
 		// Use cases for this are for example permissions error when running the `eval` command.
 		if (Reflect.get(Object(context), 'silent')) return;
+		if (identifier === Identifiers.PreconditionCooldown) {
+			let send = !cooldownMessageCooldown.has(message.author.id);
+			const { remaining } = context as { remaining: number };
+			const formattedTime = new DurationFormatter().format(remaining);
+			if (send) {
+				cooldownMessageCooldown.add(message.author.id);
+				await sendTemporaryMessage(
+					message,
+					{
+						content: `${message.author}, a little too quick there. Wait ${formattedTime ?? '1 second'}`
+					},
+					seconds(7)
+				);
+
+				setTimeout(() => cooldownMessageCooldown.delete(message.author.id), seconds(5));
+			}
+			return;
+		}
 
 		return message.reply({ content: formatFailMessage(content), allowedMentions: { users: [message.author.id], roles: [] } });
 	}
