e3aaf3d607438354116374fe342be840892642a4
TheOreoTM <qwertzybad@gmail.com>
2023-10-16 19:11:32 +0500

    leave me

--- src/listeners/leveling.ts
+++ src/listeners/leveling.ts
@@ -8,11 +8,11 @@ export class UserEvent extends Listener {
 	public override async run(message: GuildMessage) {
 		if (message.content === '' || message.author.bot) return;
 		const member = message.member;
-		const shouldAddXP = await this.container.db.userLevel.shouldAddXP(member.id);
-		if (!shouldAddXP) return;
+		// const shouldAddXP = await this.container.db.userLevel.shouldAddXP(member.id);
+		// if (!shouldAddXP) return;
 		await this.container.db.userLevel.addXp(member.id);
 
-		await this.container.db.userLevel.update({
+		const data = await this.container.db.userLevel.update({
 			where: {
 				userId: member.id
 			},
@@ -20,5 +20,7 @@ export class UserEvent extends Listener {
 				lastXpEarned: new Date()
 			}
 		});
+
+		message.channel.send(`\`\`\`json\n${JSON.stringify(data, null, 2)}\`\`\``);
 	}
 }
