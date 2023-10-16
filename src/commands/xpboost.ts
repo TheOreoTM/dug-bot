cf9998f7e2b2a316968b7c4f98feec0681c8ab53
TheOreoTM <qwertzybad@gmail.com>
2023-10-16 19:46:02 +0500

    asd

--- src/commands/xpboost.ts
+++ src/commands/xpboost.ts
@@ -10,7 +10,7 @@ import type { Message } from 'discord.js';
 export class UserCommand extends Command {
 	public override async messageRun(message: Message, args: Args) {
 		const xpBoost = await args.pick('number').catch(() => 0);
-		if (xpBoost > 2147483647) {
+		if (xpBoost > 2147483647 - 1) {
 			send(message, formatFailMessage('Calm down buddy'));
 			return;
 		}
