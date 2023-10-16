7f8fdfbb63854ab417b9631a8765beb0b112e73c
TheOreoTM <qwertzybad@gmail.com>
2023-10-16 19:39:35 +0500

    asd

--- src/commands/xpboost.ts
+++ src/commands/xpboost.ts
@@ -1,5 +1,7 @@
+import { formatFailMessage } from '#lib/util/formatter';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Args, Command } from '@sapphire/framework';
+import { send } from '@sapphire/plugin-editable-commands';
 import type { Message } from 'discord.js';
 
 @ApplyOptions<Command.Options>({
@@ -8,6 +10,10 @@ import type { Message } from 'discord.js';
 export class UserCommand extends Command {
 	public override async messageRun(message: Message, args: Args) {
 		const xpBoost = await args.pick('number').catch(() => 0);
+		if (xpBoost / 100 > 2147483647) {
+			send(message, formatFailMessage('Calm down buddy'));
+			return;
+		}
 		await this.container.db.userLevel.update({
 			where: {
 				userId: message.author.id
