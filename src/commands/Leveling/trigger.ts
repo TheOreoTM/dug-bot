91421587a2b0c6abf4d8fb26474b904f71333714
TheOreoTM <qwertzybad@gmail.com>
2023-10-24 14:06:21 +0500

    OKAYY

--- src/commands/Leveling/trigger.ts
+++ src/commands/Leveling/trigger.ts
@@ -1,12 +1,14 @@
 import { LevelingDropManager } from '#lib/classes/XpDropManager';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
+import { Message } from 'discord.js';
 
 @ApplyOptions<Command.Options>({
 	description: 'A basic command'
 })
 export class UserCommand extends Command {
-	public override async messageRun() {
+	public override async messageRun(message: Message) {
+		message.channel.send('Hi');
 		const manager = LevelingDropManager.getInstance();
 		manager.performDropLogic();
 	}
