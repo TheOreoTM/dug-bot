a2fc6877e25c195bb2ff736f9fbb6614f06bc31a
TheOreoTM <qwertzybad@gmail.com>
2023-11-02 17:55:56 +0500

    level-role remove

---
+++ src/modules/leveling/interaction-handler/level-role-remove.ts
@@ -0,0 +1,30 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { InteractionHandler, InteractionHandlerTypes } from '@sapphire/framework';
+import { AutocompleteInteraction, type ApplicationCommandOptionChoiceData } from 'discord.js';
+
+@ApplyOptions<InteractionHandler.Options>({
+	interactionHandlerType: InteractionHandlerTypes.Autocomplete
+})
+export class AutocompleteHandler extends InteractionHandler {
+	public override async run(interaction: AutocompleteInteraction, result: ApplicationCommandOptionChoiceData[]) {
+		return interaction.respond(result);
+	}
+
+	public override async parse(interaction: AutocompleteInteraction) {
+		if (interaction.commandName !== 'level-role') return this.none();
+		const focusedOption = interaction.options.getFocused(true);
+		switch (focusedOption.name) {
+			case 'level': {
+				const searchResult = await this.container.db.levelRole.findMany({
+					where: {
+						level: Number(focusedOption.value)
+					}
+				});
+				// Map the search results to the structure required for Autocomplete
+				return this.some(searchResult.map((match) => ({ name: `Level Role ${match.level}`, value: match.id })));
+			}
+			default:
+				return this.none();
+		}
+	}
+}
