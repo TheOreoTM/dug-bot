9ca3e610eea4b380e366d039d6d82c7fc268dea8
TheOreoTM <qwertzybad@gmail.com>
2023-11-02 13:54:08 +0500

    test #1 with modules

--- src/lib/DugClient.ts
+++ src/lib/DugClient.ts
@@ -1,10 +1,15 @@
 import { ClientConfig } from '#config';
 import { container, SapphireClient } from '@sapphire/framework';
 import { xprisma } from '#lib/util/prisma';
+import { getRootData } from '@sapphire/pieces';
+import { join } from 'path';
 
 export class DugClient<Ready extends boolean = boolean> extends SapphireClient<Ready> {
+	private rootData = getRootData();
 	public constructor() {
 		super(ClientConfig);
+
+		this.stores.registerPath(join(this.rootData.root, 'modules/leveling'));
 	}
 
 	public override async login(token?: string): Promise<string> {
