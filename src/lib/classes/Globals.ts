d148f5ba5d7a4a7d8fe2d7da19ade2ba6014ed3b
TheOreoTM <qwertzybad@gmail.com>
2023-11-03 11:31:34 +0500

    test 1

--- src/lib/classes/Globals.ts
+++ src/lib/classes/Globals.ts
@@ -1,9 +1,17 @@
+import { container } from '@sapphire/pieces';
+
 export class GlobalVariableClass {
 	private static instance: GlobalVariableClass;
 
-	public GlobalBoost: number = 0.5;
+	public Data = {
+		GlobalBoost: 0.0
+	};
 
-	private constructor() {} // Private constructor to prevent external instantiation
+	private constructor() {
+		this.initVariables().then(() => {
+			this.startUp();
+		});
+	} // Private constructor to prevent external instantiation
 
 	public static getInstance(): GlobalVariableClass {
 		if (!GlobalVariableClass.instance) {
@@ -12,6 +20,48 @@ export class GlobalVariableClass {
 
 		return GlobalVariableClass.instance;
 	}
+
+	private save() {
+		for (const variable in this.Data) {
+			if (Object.prototype.hasOwnProperty.call(this.Data, variable)) {
+				const value = this.Data[variable as keyof typeof this.Data];
+				container.db.globalVariable.upsert({
+					where: {
+						name: variable
+					},
+					create: {
+						name: variable,
+						value: value.toString(),
+						type: typeof value
+					},
+					update: {
+						value: value.toString(),
+						type: typeof value
+					}
+				});
+			}
+		}
+	}
+
+	private async initVariables() {
+		const variables = await container.db.globalVariable.findMany();
+		for (const variable of variables) {
+			const name = variable.name as keyof typeof this.Data;
+			if (this.Data[name] !== undefined) {
+				this.Data[name] = Number(variable.value);
+			}
+		}
+	}
+
+	private startUp() {
+		setInterval(() => {
+			this.save();
+		}, 60000);
+	}
 }
 
-export const Globals = GlobalVariableClass.getInstance();
+export enum GlobalVariablesNames {
+	GlobalBoost = 'GlobalBoost'
+}
+
+export const Globals = GlobalVariableClass.getInstance().Data;
--- src/lib/util/prisma.ts
+++ src/lib/util/prisma.ts
@@ -1,8 +1,8 @@
 import { InventoryItemType } from '#lib/types/Data';
 import { PrismaClient } from '@prisma/client';
 import { genRandomXp, getLevelInfo } from '#utils/utils';
+import { Globals } from '#lib/classes';
 const prisma = new PrismaClient();
-export let GlobalBoost = 0.2;
 
 export const xprisma = new PrismaClient().$extends({
 	name: 'xprisma',
@@ -269,6 +269,7 @@ export const xprisma = new PrismaClient().$extends({
 			},
 
 			async addXp(userId: string, options?: AddXpOptions) {
+				const GlobalBoost = Globals.GlobalBoost;
 				const xpBoost = options?.xpBoost ? options.xpBoost + GlobalBoost : GlobalBoost;
 
 				let amount: number = genRandomXp();
