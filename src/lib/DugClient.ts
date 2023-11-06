25bf5948eea33d7f9fc94a0133776a94a529795a
TheOreoTM <qwertzybad@gmail.com>
2023-11-06 16:55:44 +0500

    actually made it load the module lmao

--- src/lib/DugClient.ts
+++ src/lib/DugClient.ts
@@ -8,13 +8,12 @@ export class DugClient<Ready extends boolean = boolean> extends SapphireClient<R
 	public constructor() {
 		super(ClientConfig);
 
-		this.loadedModules = config.enabled_modules;
-
 		if (isEnabled('core')) enableModule(this, 'core');
 		if (isEnabled('leveling')) enableModule(this, 'leveling');
 		if (isEnabled('economy')) enableModule(this, 'economy');
 		if (isEnabled('faction')) enableModule(this, 'faction');
 		if (isEnabled('games')) enableModule(this, 'games');
+		if (isEnabled('welcomer')) enableModule(this, 'welcomer');
 	}
 
 	public override async login(token?: string): Promise<string> {
@@ -34,6 +33,7 @@ function isEnabled(moduleName: ModuleName) {
 function enableModule(client: DugClient, moduleName: ModuleName) {
 	const rootData = getRootData();
 
+	client.loadedModules.push(moduleName);
 	client.stores.registerPath(join(rootData.root, `modules/${moduleName}`));
 }
 
