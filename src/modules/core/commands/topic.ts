a578349eb874565c0ce289d1d55430ae7f8c62f0
TheOreoTM <qwertzybad@gmail.com>
2023-11-09 19:43:05 +0500

    topic command

---
+++ src/modules/core/commands/topic.ts
@@ -0,0 +1,26 @@
+import { DugColors } from '#constants';
+import { seconds } from '#lib/util/common';
+import { generateTopic, getTag } from '#lib/util/utils';
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, type Message } from 'discord.js';
+
+@ApplyOptions<Command.Options>({
+	description: 'A basic command',
+	cooldownDelay: seconds(10)
+})
+export class UserCommand extends Command {
+	public override async messageRun(message: Message) {
+		const newTopicButton = new ButtonBuilder().setLabel('New Topic').setStyle(ButtonStyle.Primary).setCustomId('new-topic');
+
+		const embed = new EmbedBuilder()
+			.setColor(DugColors.Default)
+			.setDescription(`Here's a **new** topic that can help...\n\n> ${generateTopic()}`)
+			.setFooter({ text: `Requested by: ${getTag(message.author)}` });
+
+		message.channel.send({
+			embeds: [embed],
+			components: [new ActionRowBuilder<ButtonBuilder>().addComponents(newTopicButton)]
+		});
+	}
+}
