4af649bac1edfef8e4b32ec3ed75a8499b142ba3
TheOreoTM <qwertzybad@gmail.com>
2023-11-18 11:51:51 +0500

    bruh

--- src/scheduled-tasks/reviveChatWithTopic.ts
+++ src/scheduled-tasks/reviveChatWithTopic.ts
@@ -30,7 +30,7 @@ export class reviveChatWithTopicTask extends ScheduledTask {
 		const message = channel.lastMessage;
 		if (!message) return true;
 		const fiveMinutesAgo = new Date(Date.now() - minutes(5));
-		const messageIsOld = message.createdTimestamp > fiveMinutesAgo.getTime();
+		const messageIsOld = message.createdTimestamp < fiveMinutesAgo.getTime();
 		const sentByMe = message.author.id === BotID;
 
 		const shouldSend = !sentByMe && messageIsOld;
