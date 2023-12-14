2805774218503c28002435ab8c39aea7bea97dd6
TheOreoTM <qwertzybad@gmail.com>
2023-12-14 08:45:08 +0500

    cold

--- src/lib/services/CoreSettingsService.ts
+++ src/lib/services/CoreSettingsService.ts
@@ -1,21 +1,24 @@
-import { globalCacheKey } from '#lib/database/keys';
+import { globalBoostCacheKey } from '#lib/database/keys';
 import { container } from '@sapphire/pieces';
 import { isNullish } from '@sapphire/utilities';
 
 export class CoreSettingsService {
-	private readonly cacheKey = globalCacheKey();
 	private readonly cache = container.cache;
 
 	public constructor() {}
 
 	public async getGlobalBoost(defaultValue = 0) {
-		const globalBoost = await this.cache.get(this.cacheKey);
+		const key = globalBoostCacheKey();
+
+		const globalBoost = await this.cache.get(key);
 		if (isNullish(globalBoost)) return defaultValue;
 		return Number(globalBoost);
 	}
 
 	public async setGlobalBoost(newValue: number) {
-		const returnValue = await this.cache.set(this.cacheKey, newValue);
+		const key = globalBoostCacheKey();
+
+		const returnValue = await this.cache.set(key, newValue);
 		return returnValue;
 	}
 }
