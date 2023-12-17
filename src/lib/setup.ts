bcc07a2e44b19abd4d5dac1897572fe84ade5154
TheOreoTM <qwertzybad@gmail.com>
2023-12-17 16:55:23 +0500

    bro

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -2,10 +2,10 @@
 process.env.NODE_ENV ??= 'development';
 
 import { ApplicationCommandRegistries, RegisterBehavior } from '@sapphire/framework';
+import '@sapphire/plugin-api/register';
 import '@sapphire/plugin-logger/register';
 import '@sapphire/plugin-subcommands/register';
 import '@sapphire/plugin-scheduled-tasks/register';
-import '@sapphire/plugin-api/register';
 import { setup } from '@skyra/env-utilities';
 import * as colorette from 'colorette';
 
