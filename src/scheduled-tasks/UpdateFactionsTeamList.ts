6a8395627b7d3e1880fe5db85f23f5442d46c44a
TheOreoTM <qwertzybad@gmail.com>
2023-12-21 20:05:46 +0500

    and i do my thing

--- src/scheduled-tasks/UpdateFactionsTeamList.ts
+++ src/scheduled-tasks/UpdateFactionsTeamList.ts
@@ -1,4 +1,3 @@
-import { config } from '#config';
 import { minutes } from '#lib/util/common';
 import { ApplyOptions } from '@sapphire/decorators';
 import { ScheduledTask } from '@sapphire/plugin-scheduled-tasks';
@@ -6,11 +5,15 @@ import { ScheduledTask } from '@sapphire/plugin-scheduled-tasks';
 @ApplyOptions<ScheduledTask.Options>({
 	name: 'UpdateFactionsTeamListTask',
 	interval: minutes(2.5),
-	enabled: config.enabled_modules.includes('faction'),
 	bullJobsOptions: { removeOnComplete: true }
 })
 export class UpdateFactionsTeamListTask extends ScheduledTask {
 	public async run() {
+		if (!this.container.client.loadedModules.includes('faction')) return;
+		console.log(
+			"🚀 ~ file: UpdateFactionsTeamList.ts:14 ~ UpdateFactionsTeamListTask ~ run ~ this.container.client.loadedModules.includes('faction'):",
+			this.container.client.loadedModules.includes('faction')
+		);
 		this.container.logger.info('[UpdateFactionsTeamListTask] Started');
 		await this.container.faction.list.refreshList();
 	}
