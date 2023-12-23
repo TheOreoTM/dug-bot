c6fb3507d6298abd37ad5d4a914f43406ae5b98f
TheOreoTM <qwertzybad@gmail.com>
2023-12-24 00:54:47 +0500

    um okay

---
+++ src/modules/logging/listeners/message/guildMessageDeleteImageNotify.ts
@@ -0,0 +1,26 @@
+import { DugEvents, LoggingWebhooks, ZeroWidthSpace } from '#constants';
+import { GuildMessage } from '#lib/types';
+import { getContent, getFullEmbedAuthor, getImage } from '#lib/util/utils';
+import { ApplyOptions } from '@sapphire/decorators';
+import { Listener } from '@sapphire/framework';
+import { cutText } from '@sapphire/utilities';
+import { Colors, EmbedBuilder } from 'discord.js';
+
+@ApplyOptions<Listener.Options>({
+	event: DugEvents.GuildMessageDelete
+})
+export class UserEvent extends Listener {
+	public async run(message: GuildMessage) {
+		this.container.core.logging.sendLog(LoggingWebhooks.Message, {
+			embeds: [
+				new EmbedBuilder()
+					.setColor(Colors.Red)
+					.setAuthor(getFullEmbedAuthor(message.author, message.url))
+					.setDescription(cutText(getContent(message) || ZeroWidthSpace, 1900))
+					.setFooter({ text: `Message Deleted • #${message.channel.name}` })
+					.setImage(getImage(message)!)
+					.setTimestamp()
+			]
+		});
+	}
+}
