aa94d34aa73b2a00d9d9a146bcf330200c8593f1
TheOreoTM <qwertzybad@gmail.com>
2023-12-26 02:29:48 +0500

    made it not leak hints to the web

--- src/routes/event/ciphers.ts
+++ src/routes/event/ciphers.ts
@@ -1,4 +1,4 @@
-import { CIPHER_DATA, CipherData } from '#lib/data';
+import { CIPHER_DATA } from '#lib/data';
 import { ApplyOptions } from '@sapphire/decorators';
 import { methods, Route, type ApiRequest, type ApiResponse } from '@sapphire/plugin-api';
 
@@ -9,9 +9,9 @@ export class UserRoute extends Route {
 	public async [methods.GET](_request: ApiRequest, response: ApiResponse) {
 		const unlockedSet = await this.container.cipher.getUnlockedSet();
 		const unlockedArray = Array.from(unlockedSet);
-		const ciphers: (CipherData & { LEVEL: number })[] = [];
+		const ciphers: { DESCRIPTION: string; LEVEL: number }[] = [];
 		for (const cipher of unlockedArray) {
-			ciphers.push({ ...CIPHER_DATA[`CIPHER_${cipher}`], LEVEL: cipher });
+			ciphers.push({ DESCRIPTION: CIPHER_DATA[`CIPHER_${cipher}`].DESCRIPTION, LEVEL: cipher });
 		}
 		response.json({ ciphers });
 	}
