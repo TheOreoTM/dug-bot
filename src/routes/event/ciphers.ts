a435a661fb55fd8eb4258f6e9d306e37694e0f02
TheOreoTM <qwertzybad@gmail.com>
2023-12-26 13:11:57 +0500

    feat: api key

--- src/routes/event/ciphers.ts
+++ src/routes/event/ciphers.ts
@@ -2,6 +2,7 @@ import { API_URI } from '#constants';
 import { CipherData, CipherLevel } from '#lib/data';
 import { ApplyOptions } from '@sapphire/decorators';
 import { methods, Route, type ApiRequest, type ApiResponse } from '@sapphire/plugin-api';
+import { envParseString } from '@skyra/env-utilities';
 
 @ApplyOptions<Route.Options>({
 	route: 'event/ciphers'
@@ -10,7 +11,7 @@ export class UserRoute extends Route {
 	public async [methods.GET](_request: ApiRequest, response: ApiResponse) {
 		const unlockedSet = await this.container.cipher.getUnlockedSet();
 		const unlockedArray = Array.from(unlockedSet);
-		const cipherDataRes = await fetch(`${API_URI}/ciphers`);
+		const cipherDataRes = await fetch(`${API_URI}/ciphers?key=${envParseString('API_KEY')}`);
 		const cipherData = (await cipherDataRes.json()) as Record<CipherLevel, CipherData>;
 		const ciphers: { DESCRIPTION: string; LEVEL: number }[] = [];
 		for (const cipher of unlockedArray) {
