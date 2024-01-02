094b936c15360434f95ed5d7857b74e3bfcf669a
theoreotm <qwertzybad@gmail.com>
2024-01-02 21:30:59 +0500

    feat: get leveling settings

---
+++ src/routes/settings/leveling.ts
@@ -0,0 +1,18 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { methods, Route, type ApiRequest, type ApiResponse } from '@sapphire/plugin-api';
+
+@ApplyOptions<Route.Options>({
+	route: 'settings/route'
+})
+export class UserRoute extends Route {
+	public async [methods.GET](_request: ApiRequest, response: ApiResponse) {
+		const levelRoles = await this.container.db.levelRole.findMany({ orderBy: { level: 'asc' } });
+		const globalBoost = await this.container.core.getGlobalBoost();
+
+		response.json({ levelRoles, globalBoost });
+	}
+
+	public [methods.POST](_request: ApiRequest, response: ApiResponse) {
+		response.json({ message: 'Hello World' });
+	}
+}
