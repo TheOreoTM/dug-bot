eb5b56da7715defe46a7e94913bd8bf241c97009
theoreotm <qwertzybad@gmail.com>
2024-01-08 19:09:05 +0500

    feat: dbuging logs

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -9,10 +9,12 @@ import { send } from '@sapphire/plugin-editable-commands';
 export class UserCommand extends DugCommand {
 	// Message command
 	public override async messageRun(message: DugCommand.Message) {
+		console.log('hi');
 		const handler = await this.container.pet.getUserPetHandler(message.author.id);
 		const pets = await handler.getPets();
 
 		const embed = new PetListEmbedBuilder(pets).build();
+		console.log('bye');
 
 		send(message, { embeds: [embed] });
 	}
