8e7131c1541d37e9f16ae137fb7898f3ac8655d8
theoreotm <qwertzybad@gmail.com>
2024-01-10 16:33:16 +0500

    refactor: user level route to include authentication and error handling

--- src/routes/levels/[user].ts
+++ src/routes/levels/[user].ts
@@ -1,5 +1,8 @@
+import { SendLogEmbed } from '#lib/classes';
+import { authenticated } from '#lib/util/api';
+import { getLevelInfo } from '#lib/util/utils';
 import { ApplyOptions } from '@sapphire/decorators';
-import { methods, Route, type ApiRequest, type ApiResponse } from '@sapphire/plugin-api';
+import { methods, Route, type ApiRequest, type ApiResponse, HttpCodes } from '@sapphire/plugin-api';
 
 @ApplyOptions<Route.Options>({
 	route: 'levels/:user'
@@ -22,7 +25,63 @@ export class UserRoute extends Route {
 		return response.json({ ...userData, rank });
 	}
 
-	public [methods.POST](_request: ApiRequest, response: ApiResponse) {
-		response.json({ message: 'Hello World' });
+	@authenticated()
+	public async [methods.POST](request: ApiRequest, response: ApiResponse) {
+		const requestBody = request.body as { amount: string | undefined; user_id: string | undefined; staff_id: string | undefined };
+		const amountRes = requestBody['amount'];
+		const userId = requestBody['user_id'];
+		const staffId = requestBody['staff_id'];
+
+		console.log(requestBody);
+		console.log(amountRes, userId, staffId);
+		console.log(requestBody.amount, requestBody.user_id, requestBody.user_id);
+
+		if (!amountRes) {
+			return response.status(HttpCodes.BadRequest).json({ message: 'Invalid amount' });
+		}
+
+		const amount = parseInt(amountRes);
+		if (isNaN(amount)) {
+			return response.status(HttpCodes.BadRequest).json({ message: 'Invalid amount, is not a number' });
+		}
+
+		if (!staffId) {
+			return response.status(HttpCodes.BadRequest).json({ message: 'Invalid staff_id' });
+		}
+		if (!userId) {
+			return response.status(HttpCodes.BadRequest).json({ message: 'Invalid Body' });
+		}
+
+		const levelData = getLevelInfo(amount);
+
+		await this.container.db.userLevel.upsert({
+			where: {
+				userId: userId
+			},
+			create: {
+				userId: userId,
+				currentLevel: amount,
+				currentXp: 0,
+				totalXp: levelData.totalXpOfCurrentLevel,
+				requiredXp: levelData.xpNeededToLevelUp
+			},
+			update: {
+				currentLevel: amount,
+				currentXp: 0,
+				totalXp: levelData.totalXpOfCurrentLevel,
+				requiredXp: levelData.xpNeededToLevelUp
+			}
+		});
+
+		const user = await this.container.client.users.fetch(userId);
+		const staff = await this.container.client.users.fetch(staffId);
+		SendLogEmbed.LevelSet({
+			user: user,
+			level: amount,
+			reason: '`xp set level` command **through API**',
+			staff: staff
+		});
+
+		return response.json({ message: 'success' });
 	}
 }
