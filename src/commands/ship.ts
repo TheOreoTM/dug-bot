a2cc57b21e2542d790bb6b6906698dbb7dc6dec1
theoreotm <qwertzybad@gmail.com>
2024-01-12 21:51:55 +0500

    feat: percentage

--- src/commands/ship.ts
+++ src/commands/ship.ts
@@ -1,6 +1,8 @@
 import { DugCommand } from '#lib/structures';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Args } from '@sapphire/framework';
+import { createHash } from 'crypto';
+import { User } from 'discord.js';
 
 @ApplyOptions<DugCommand.Options>({
 	description: 'ADD'
@@ -10,13 +12,26 @@ export class UserCommand extends DugCommand {
 		const first = await args
 			.pick('user')
 			.catch(() => args.pick('string'))
-			.catch(() => null);
+			.catch(() => 'null');
 		const second = await args
 			.pick('user')
 			.catch(() => args.pick('string'))
-			.catch(() => null);
+			.catch(() => 'null');
 
-		message.channel.send(`I ship ${first} and ${second}!`);
+		const compatibility = this.generatePercentage(
+			first instanceof User ? first.username : first,
+			second instanceof User ? second.username : second
+		);
+
+		message.channel.send(`I ship ${first} and ${second}! ${compatibility}`);
 		return;
+	}
+
+	private generatePercentage(first: string, second: string): number {
+		const input = `${first}-${second}`;
+		const hash = createHash('md5').update(input).digest('hex');
+		const decimal = parseInt(hash.slice(0, 2), 16) / 255;
+		const percentage = Math.round(decimal * 100);
+		return percentage;
 	}
 }
