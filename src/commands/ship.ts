2601eddf68921f56b11efe669cbc5fb3a6f0a1e0
theoreotm <qwertzybad@gmail.com>
2024-01-12 21:37:06 +0500

    feat: simple sapphire code

--- src/commands/ship.ts
+++ src/commands/ship.ts
@@ -7,25 +7,15 @@ import { Collection, GuildMember } from 'discord.js';
 	description: 'ADD'
 })
 export class UserCommand extends DugCommand {
-	public override async messageRun(message: DugCommand.Message, _args: Args) {
-		const args = message.content.split(' ');
-		const mentions: Collection<string, GuildMember> = message.mentions.members ?? new Collection();
-
-		let first: string | null = null;
-		let second: string | null = null;
-
-		if (args.length === 1) {
-			first = message.author.username;
-			second = mentions.first()?.user.username ?? null;
-		} else if (args.length === 2) {
-			first = message.author.username;
-			second = args[1];
-		} else if (args.length === 3) {
-			first = args[1];
-			second = args[2];
-		} else {
-			return message.channel.send('Invalid syntax! Try `ship @user` or `dug ship @user1 @user2`');
-		}
+	public override async messageRun(message: DugCommand.Message, args: Args) {
+		const first = args
+			.pick('member')
+			.catch(() => args.pick('string'))
+			.catch(() => null);
+		const second = args
+			.pick('member')
+			.catch(() => args.pick('string'))
+			.catch(() => null);
 
 		message.channel.send(`I ship ${first} and ${second}!`);
 		return;
