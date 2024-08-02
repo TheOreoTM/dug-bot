76ba1d5d197cbfc2ad5bc47513b7c8bfe116a065
TheOreoTM <qwertzybad@gmail.com>
2024-08-02 11:48:08 +0500

    feat: remove precondition

--- src/modules/faction/commands/faction-join.ts
+++ src/modules/faction/commands/faction-join.ts
@@ -3,8 +3,7 @@ import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
 
 @ApplyOptions<Command.Options>({
-	description: 'Join a faction',
-	preconditions: ['EventManager']
+	description: 'Join a faction'
 })
 export class UserCommand extends Command {
 	public override registerApplicationCommands(registry: Command.Registry) {
