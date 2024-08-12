9f3a635f6693a2814ad647543ee65d57b3a45aa8
TheOreoTM <qwertzybad@gmail.com>
2024-08-12 21:17:56 +0500

    feat: Add announcement role ping

--- src/commands/task.ts
+++ src/commands/task.ts
@@ -1,12 +1,22 @@
 import { DugCommand } from '#lib/structures';
 import { ApplyOptions } from '@sapphire/decorators';
+import type { Message } from 'discord.js';
 
 @ApplyOptions<DugCommand.Options>({
-	description: 'ADD'
+	description: 'ADD',
+	requiredUserPermissions: 'Administrator'
 })
 export class UserCommand extends DugCommand {
-	public override async messageRun() {
-		this.container.tasks.create('ExpireBoostsTask');
+	public override async messageRun(message: Message) {
+		const members = await message.guild?.members.fetch();
+		let counter = 0;
+		members?.forEach((member) => {
+			counter++;
+			member.roles.add('1272577465193205800');
+			console.log(`Added ${member.user.username} to the role (${counter / members.size})`);
+		});
+
+		message.channel.send(`Added ${counter} users to the role`);
 		return;
 	}
 }
